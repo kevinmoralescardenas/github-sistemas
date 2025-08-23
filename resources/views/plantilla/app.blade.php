0a12ed1f3cd3e4d544f2472c56d9c7200d5a26e6
kevin morales <kevinmoralesk123@gmail.com>
2025-08-23 09:57:37 -0500

    se cambio el idioma de ingles a español

--- resources/views/plantilla/app.blade.php
+++ resources/views/plantilla/app.blade.php
@@ -1,9 +1,9 @@
 <!doctype html>
-<html lang="en">
+<html lang="es">
   <!--begin::Head-->
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>AdminLTE 4 | Simple Tables</title>
+    <title>panel de control | Sistema</title>
     <!--begin::Accessibility Meta Tags-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes" />
     <meta name="color-scheme" content="light dark" />
@@ -11,7 +11,7 @@
     <meta name="theme-color" content="#1a1a1a" media="(prefers-color-scheme: dark)" />
     <!--end::Accessibility Meta Tags-->
     <!--begin::Primary Meta Tags-->
-    <meta name="title" content="AdminLTE 4 | Simple Tables" />
+    <meta name="title" content="panel control| sistema" />
     <meta name="author" content="ColorlibHQ" />
     <meta
       name="description"
